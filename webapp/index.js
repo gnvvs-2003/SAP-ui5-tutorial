d44a624745eef688e90303cbfe547d139e14800d
gnvvs-2003 <vchess007@gmail.com>
2026-04-23 11:41:59 +0530

    Added a text control

--- webapp/index.js
+++ webapp/index.js
@@ -3,10 +3,20 @@
  * @notice This is the entry point of the application.
  */
 
-/// Sample function
+/// Adding text using sap Text controller
+
+/**
+ * @description : This function is used to add text to the application.
+ * @param {sap.m.Text} Text - The text to be added to the application.
+ * @returns {void} - This function does not return any value.
+ * @notice : This text is placed at the id = content (i.e body) directly
+ */
+
 sap.ui.define(
-    [],()=>{
+    ["sap/m/Text"],(Text)=>{
         "use strict";
-        alert("Hello World !!!");
+        new Text({
+            text:"Hello World!!!"
+        }).placeAt("content");
     }
 )
