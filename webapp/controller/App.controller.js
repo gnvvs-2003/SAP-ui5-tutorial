4a22e8c4398afbe17a5d1069132447a04cebf623
gnvvs-2003 <vchess007@gmail.com>
2026-04-23 18:03:44 +0530

    implement HelloPanel component with custom styling and controller logic

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,20 +5,11 @@
 
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast",
-    ],(Controller,MessageToast)=>{
+        "sap/ui/core/mvc/Controller"
+    ],(Controller)=>{
         "use strict";
         return Controller.extend("ui5.walkthrough.controller.App",{
-            /// Functions used in the "App.view.xml"
-            onShowHello : function(){
-                // Reads msg from i18n model
-                const oBundle = this.getView().getModel("i18n").getResourceBundle();
-                const sRecipient = this.getView().getModel().getProperty("/recipient/name");// gets name from Json model
-                const sMsg = oBundle.getText("helloMsg",[sRecipient]);
-                MessageToast.show(sMsg);
-            }
-
-        })
+            
+        });
     }
-)
+);
