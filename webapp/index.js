3f7980b2b2726803a25f125c1525e0f6dc400c61
gnvvs-2003 <vchess007@gmail.com>
2026-04-23 11:29:18 +0530

    Added sample function and ui5 cli setup

---
+++ webapp/index.js
@@ -0,0 +1,12 @@
+/**
+ * @author : gnvvs-2003
+ * @notice This is the entry point of the application.
+ */
+
+/// Sample function
+sap.ui.define(
+    [],()=>{
+        "use strict";
+        alert("Hello World !!!");
+    }
+)
