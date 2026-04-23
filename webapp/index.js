f3e884da3f14edbccbe0dd6043dabb37199ee0fb
gnvvs-2003 <vchess007@gmail.com>
2026-04-23 12:04:35 +0530

    Loaded view into index.js

--- webapp/index.js
+++ webapp/index.js
@@ -3,20 +3,22 @@
  * @notice This is the entry point of the application.
  */
 
-/// Adding text using sap Text controller
+/// Connecting App.view.xml for Text 
 
 /**
- * @description : This function is used to add text to the application.
- * @param {sap.m.Text} Text - The text to be added to the application.
+ * @description : This function is used to add text to the application by connecting App.view.xml file  
+ * @param {sap.ui.core.mvc.XMLView} XMLView - The XMLView to be added to the application.
  * @returns {void} - This function does not return any value.
  * @notice : This text is placed at the id = content (i.e body) directly
  */
 
 sap.ui.define(
-    ["sap/m/Text"],(Text)=>{
+    ["sap/ui/core/mvc/XMLView"],(XMLView)=>{
         "use strict";
-        new Text({
-            text:"Hello World!!!"
-        }).placeAt("content");
+        new XMLView.create({
+            viewName:"ui5.walkthrough.view.App" // The name will be (ui5/walkthrough)(name space)/view/App....
+        }).then((oView)=>{
+            oView.placeAt("content"); // id = content (body)
+        });
     }
 )
