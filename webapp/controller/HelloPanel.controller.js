63d7abb6596f018c543dce391d5c6fd8b4671265
gnvvs-2003 <vchess007@gmail.com>
2026-04-23 18:18:58 +0530

    fragments,callbacks,nested views completed

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -5,13 +5,26 @@ sap.ui.define(
     ], (Controller, MessageToast) => {
         "use strict";
         return Controller.extend("ui5.walkthrough.controller.App", {
-            /// Functions used in the "App.view.xml"
             onShowHello: function () {
                 // Reads msg from i18n model
                 const oBundle = this.getView().getModel("i18n").getResourceBundle();
                 const sRecipient = this.getView().getModel().getProperty("/recipient/name");// gets name from Json model
                 const sMsg = oBundle.getText("helloMsg", [sRecipient]);
                 MessageToast.show(sMsg);
+            },
+            async onOpenDialog() {
+                // create dialog lazily
+                this.oDialog ??= await this.loadFragment({
+                    name: "ui5.walkthrough.view.HelloDialog"
+                });
+
+                this.oDialog.open();
+            },
+
+            onCloseDialog() {
+                // note: We don't need to chain to the pDialog promise, since this event handler
+                // is only called from within the loaded dialog itself.
+                this.byId("helloDialog").close();
             }
 
         })
