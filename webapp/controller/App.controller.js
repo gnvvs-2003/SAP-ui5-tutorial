570d6cac095f1d10b7e8a70c4aeb9ca74eadf981
gnvvs-2003 <vchess007@gmail.com>
2026-04-23 13:08:48 +0530

    Added controller for button

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+/**
+ * @author gnvvs-2003
+ * This is the controller for all the elements present in App.view.xml
+ */
+
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller"],(Controller)=>{
+        "use strict";
+        return Controller.extend("ui5.walkthrough.controller.App",{
+            //functions defined in the "App.view.xml"
+            onShowHello : function(){
+                alert("Hello World!!!");
+            }
+        })
+    }
+)
